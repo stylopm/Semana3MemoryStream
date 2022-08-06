6331b3091b4c6b4e5d7bcf33bc60d3b26a16a0b7
Luis Antonio Peña Murillo <apmurillo99@hotmail.com>
2022-08-06 04:29:59 -0500

    ideas

--- MemoryStream/Program.cs
+++ MemoryStream/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.IO;
+using System.Text;
 
 namespace MemoryStream
 {
@@ -6,7 +9,15 @@ namespace MemoryStream
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string writeText = "Hello World de nuevo!";  // Create a text string
+            File.WriteAllText("filename.txt", writeText);  // Create a file and write the content of writeText to it
+
+            string readText = File.ReadAllText("filename.txt");  // Read the contents of the file
+            Console.WriteLine("**************");  // Output the content
+
+            Console.WriteLine(readText);  // Output the content
+
+            Console.WriteLine("--------------");  // Output the content
         }
     }
 }
